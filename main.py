c683828f350156ff2d73ec164b59bdc1109fd009
yamap55 <1785817+yamap55@users.noreply.github.com>
2024-05-12 14:49:05 +0900

    add code

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 """main"""
+import tkinter as tk
+
 
 def main():
-    print("Hello, World!")
+    """メイン関数"""
+    # メインウィンドウを作成
+    root = tk.Tk()
+    root.title("simple desktop app title")
+
+    # ラベルウィジェットを作成して配置
+    label = tk.Label(root, text="HELLO WORLD", font=("Arial", 24))
+    label.pack(expand=True)
+
+    # GUIを開始
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
